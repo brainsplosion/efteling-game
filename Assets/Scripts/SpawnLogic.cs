f63030a79910fe2340db694d6d4d495dadb20f17
mircobraams <mircobraams@icloud.com>
2024-05-13 11:58:11 +0200

    do

--- Assets/Scripts/SpawnLogic.cs
+++ Assets/Scripts/SpawnLogic.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SpawnLogic : MonoBehaviour
 {
 
     public List<GameObject> spawners;
     public List<GameObject> enemies;
+    private List<GameObject> surviving;
+
+    private bool activeWave()
+    {
+        surviving = surviving.Where(e => e != null).ToList();
+        return surviving.Count > 0;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +24,15 @@ public class SpawnLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
     private void Spawn(int species)
     {
-        int randomSpot = Random.Range(0, spawners.Count + 1);
+        int randomSpot = Random.Range(0, spawners.Count);
         Vector3 location = new Vector3(spawners[randomSpot].transform.position.x, spawners[randomSpot].transform.position.y, spawners[randomSpot].transform.position.z);
         GameObject currentEnemy = Instantiate(enemies[species], location, Quaternion.identity);
+        surviving.Add(currentEnemy);
     }
 }
