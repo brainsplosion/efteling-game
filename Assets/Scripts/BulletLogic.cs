84bfffa93e1b7f365c084a161963d46535e5b8ad
mircobraams <mircobraams@icloud.com>
2024-06-04 11:35:30 +0200

    shoot done

---
+++ Assets/Scripts/BulletLogic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour
+{
+    private float bulletSpeed = 10f;
+    private float bulletDuration = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+        transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+        bulletDuration -= Time.deltaTime;
+        if (bulletDuration <= 0)
+            Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var atm = other.GetComponent<AttributesManager>();
+            atm.TakeDamage(10);
+        }
+    }
+
+}
---
+++ Assets/Scripts/EnemyFly.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class EnemyFly : EnemyBase
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        GameObject tempTarget = GameObject.FindGameObjectWithTag("Player");
+        Player = tempTarget.transform;
+        nav = GetComponent<NavMeshAgent>();
+        nav.speed = MoveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Vector3.Distance(transform.position, Player.position) <= MinDist)
+        {
+            transform.LookAt(Player);
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            if (Vector3.Distance(transform.position, Player.position) <= 1)
+            {
+                Knockback(3);
+            }
+        }*/
+        /*transform.LookAt(new Vector3(Player.position.x, transform.position.y, Player.position.z));
+        /*transform.position += transform.forward * MoveSpeed * Time.deltaTime;*/
+        nav.destination = Player.position;
+        if (Vector3.Distance(transform.position, Player.position) <= 1)
+        {
+            Knockback(3);
+        }
+
+
+    }
+
+    public void Knockback(int strength)
+    {
+        KDirect = new Vector3(transform.position.x - Player.position.x, 0, transform.position.z - Player.position.z);
+        KDirect = KDirect.normalized * strength;
+        body.AddForce(KDirect, ForceMode.Impulse);
+        //body.AddForce(Player.position - transform.position, ForceMode.Force);
+        //transform.position -= transform.forward * Distance;
+    }
+}
